1a34e371ad0b387d09110a43febea984495f4af2
EDZ <1012256310@qq.com>
2018-11-20 16:27:04 +0800

    demand2

--- GitLearn/AppDelegate.m
+++ GitLearn/AppDelegate.m
@@ -35,6 +35,10 @@
     NSLog(@"demand1");
 }
 
+- (void)demand2{
+    NSLog(@"demand2");
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
